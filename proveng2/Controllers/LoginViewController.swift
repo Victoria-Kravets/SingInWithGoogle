a06e3f0e1559978cf6dce94025d58964d8afc779
pavel <pavel>
2017-08-22 21:04:13 +0300

    update

--- proveng2/Controllers/LoginViewController.swift
+++ proveng2/Controllers/LoginViewController.swift
@@ -9,19 +9,16 @@
 import Foundation
 import UIKit
 
-class LoginViewController: BaseViewController, GIDSignInUIDelegate {
+class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: BaseButton!
     @IBOutlet weak var rulesLabelTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var rulesLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var signInButton: GIDSignInButton!
-    lazy var serviceAuth = ServiceAuth()
-    lazy var serviceRequest = ServiceForRequest<User>()
-    let googleSignIn = GIDSignIn.sharedInstance()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.googleSignIn?.uiDelegate = self
     }
 
 
