e41349761b0c0acdea390215651275598d2bc2e7
AntonKuznetsov31 <anton.kuznetsov.mail@gmail.com>
2017-08-23 22:06:25 +0300

    Correct constraints in Login screen

---
+++ proveng2/Controllers/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  proveng2
+//
+//  Created by pavel on 8/22/17.
+//  Copyright © 2017 Provectus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: BaseButton!
+    @IBOutlet weak var rulesLabelTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rulesLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var signInButton: BaseButton! // Has to be GIDSignInButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
