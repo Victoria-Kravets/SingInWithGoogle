8a6e7db55e80b3f1781e2b1a135e8761178d256c
pavel <pavel>
2017-08-27 12:11:22 +0300

    second

---
+++ proveng2/Utilities/provectus2-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  provectus2-Bridging-Header.h
+//  proveng2
+//
+//  Created by Pavel Nikitinsky on 8/27/17.
+//  Copyright © 2017 Provectus. All rights reserved.
+//
+
+
+//#import <GoogleSignIn/GoogleSignIn.h>
+
